c6ed115a260e12a8c4cc1bb1e4c50b16f9f6c127
Jamie Knight <jamie@jkg3.com>
2012-06-17 20:54:29 +0100

    Support for categories added to default form

--- default_form.php
+++ default_form.php
@@ -1,14 +1,30 @@
 <article>
-	<txp:if_article_image>
-		<txp:article_image   />
+<div>
+
+<txp:if_article_image>
+	<txp:article_image  class="post-image" />
+<txp:else />
+	<txp:if_article_category name="raspberry-pi">
+		<img src="/assets/raspberry_pi_article_logo.jpg" alt="" class="post-image"/>
+	</txp:if_article_category>
+</txp:if_article_image>
+
+ 
+	<txp:if_individual_article>
+		<h1><txp:title /></h1>
 	<txp:else />
-		<txp:if_article_category name="raspberry-pi">
-			<img src="/assets/raspberry_pi_article_logo.jpg" alt="" class="post-image"/>
-		</txp:if_article_category>
-	</txp:if_article_image>
 		<h1><txp:permlink><txp:title /></txp:permlink></h1>
+	</txp:if_individual_article>
+
 	<txp:body />
-	
-		<p class="meta">Published: <txp:posted /> , <txp:permlink>Permalink</txp:permlink>
+
+	<p class="meta">
+		Published: <txp:posted /> 
+		<txp:chh_if_data>
+                    | Categories: <txp:category1 link="1" title="1" />
+		 <txp:chh_if_data>, <txp:category2 link="1" title="1" /></txp:chh_if_data>
+		</txp:chh_if_data>
+		<txp:permlink class="permlink">Permalink</txp:permlink>
 	</p>
+</div>	
 </article>
