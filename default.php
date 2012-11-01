c6d302eb0869ba0a98c61b3de075dccf38b323bb
Jamie Knight <jamie@jkg3.com>
2012-11-01 14:24:07 +0000

    Tidy Up.
    Nicer HTML output.

--- default.php
+++ default.php
@@ -6,6 +6,54 @@
 	<txp:feed_link format="link" flavor="rss" />
 	<meta name="viewport" content="width=device-width" />
 	<meta charset="UTF-8">
+</head>
+
+<body id="home">
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/archive">Archive</a></li>
+			<li><a href="http://pluslion.com">+Lion </a></li>
+			<txp:feed_link flavor="rss" wraptag="li"><img src="/assets/rss.png" alt="RSS Feed" /> </txp:feed_link>
+		</ul>
+	</nav>
+	
+	<aside>
+		<a href="/">
+			<img src="/jamie_and_lion.jpg" alt="Photo of Jamie &amp; Lion" />
+		</a>
+		<p>The personal site of <strong>Jamie Knight</strong>, a slightly autistic (<abbr title="British Boradcasting Corpoeration">BBC</abbr>)  <strong>web developer</strong>, <strong>speaker</strong> and <strong>mountain biker</strong> who is never seen far from his plush sidekick <em>Lion</em>.</p>
+	</aside>
+	
+	<txp:if_section name=", Journal"> 
+		<txp:if_category>
+			<div id="archive">
+				<txp:article_custom category='<txp:category_list parent=''<txp:category />'' break=""><txp:category /><txp:if_last_category><txp:else />,</txp:if_last_category></txp:category_list>' form="monthly_article" />
+			</div>
+		<txp:else />
+			<txp:if_individual_article>
+				<txp:article />
+			<txp:else />
+				<txp:article_custom section="journal" limit="3" />
+			</txp:if_individual_article>		
+		</txp:if_category>
+	</txp:if_section>
+
+
+	<txp:if_section name="archive">
+		<div id="archive">
+			<txp:if_individual_article> 		
+				<txp:article />
+			<txp:else />
+				<txp:article_custom limit="99999" form="monthly_article" section="journal" /> 
+			</txp:if_individual_article>
+		</div>
+	</txp:if_section>
+
+	<txp:if_section name="lickr">
+		<txp:article />
+	</txp:if_section>
+          
 	<script type="text/javascript">
 	  //Please edit this tracking code if you use this theme.
 	  var _gaq = _gaq || [];
@@ -17,64 +65,6 @@
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
-	
 	</script>
-</head>
-
-<body id="home">
-
-<nav>
-	<ul>
-<!-- 	<li>Pages</li> -->
-	<li><a href="/">Home</a></li>
-<!-- 	<li><a href="/about">About</a></li> -->
-	<li><a href="/archive">Archive</a></li>
-<!--
-	
-	<li class="other">Other</li>
--->
-	<li><a href="http://pluslion.com">+Lion </a></li>
-	<txp:feed_link flavor="rss" wraptag="li"><img src="/assets/rss.png" alr="RSS Feed" /> </txp:feed_link>
-	</ul>
-</nav>
-
-<aside>
-	<a href="/">
-		<img src="/jamie_and_lion.jpg" alt="Photo of Jamie &amp; Lion" />
-	</a>
-	<p>The personal site of <strong>Jamie Knight</strong>, a slightly autistic (<abbr title="British Boradcasting Corpoeration">BBC</abbr>)  <strong>web developer</strong>, <strong>speaker</strong> and <strong>mountain biker</strong> who is never seen far from his plush sidekick <em>Lion</em>.</p>
-</aside>
-
-
-<txp:if_section name=", Journal"> 
-	
-	<txp:if_category>
-		<div id="archive">
-		<txp:article_custom category='<txp:category_list parent=''<txp:category />'' break=""><txp:category /><txp:if_last_category><txp:else />,</txp:if_last_category></txp:category_list>' form="monthly_article" />
-		</div>
-	<txp:else />
-		<txp:if_individual_article>
-			<txp:article />
-		<txp:else />
-			<txp:article_custom section="journal" limit="3" />
-		</txp:if_individual_article>
-		
-	</txp:if_category>
-</txp:if_section>
-
-
-<txp:if_section name="archive">
-	<div id="archive">
-		<txp:if_individual_article> 		
-			<txp:article />
-			
-			<txp:else />
-			
-			<txp:article_custom limit="99999" form="monthly_article" section="journal" /> 
-		</txp:if_individual_article>
-	</div>
-</txp:if_section>
-          
-
 </body>
 </html>
