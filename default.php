5cde56995f9b5c140949622285385270cf354d9f
Jamie Knight <jamie@jkg3.com>
2012-11-05 20:56:48 +0000

    Change Photos for something nicer :D

--- default.php
+++ default.php
@@ -22,7 +22,7 @@
 	
 	<aside>
 		<a href="/">
-			<img src="/jamie_and_lion.jpg" alt="Photo of Jamie &amp; Lion" />
+			<img src="/headshot.jpg" alt="Photo of Jamie &amp; Lion" />
 		</a>
 		<p>The personal site of <strong>Jamie Knight</strong>, a slightly autistic (<abbr title="British Boradcasting Corpoeration">BBC</abbr>)  <strong>web developer</strong>, <strong>speaker</strong> and <strong>mountain biker</strong> who is never seen far from his plush sidekick <em>Lion</em>.</p>
 	</aside>
